bb5afca5141edf22086d22da89be216bf652296a
LizvethM <morales-garciaLizveth2021@email.com>
2020-08-03 17:16:18 +0000

    paddle_LizvethM

--- main.py
+++ main.py
@@ -4,37 +4,52 @@ import os
 img_path = os.path.join('paddle.png')
 
 class paddle(object):
-  def __int__(self):
-    paddle.image = pygame.image.load("paddle.png")
-    self.image = paddle.image
-#    self.image = pygame.transform.scale(self.image(50,50))
+  def __init__(self):
+      pygame.sprite.Sprite.__init__(self)
+      paddle.image = pygame.image.load("paddle.png")
 
-    self.x = 50
-    self.y = 50
-    self.hitbox = (self.x,self.y,55, 55)
+      self.x = 60
+      self.y = 60
+      self.hitbox = (self.x,self.y,34, 34)
+
+
 
   def draw(self, surface):
     surface.blit(self.image,(self.x,self.y))
+    self.image = paddle.image
+    #self.paddle = pygame.transform.scale(self.image(32,60))
+ 
+    
+
 
   def movement(self):
     key = pygame.key.get_pressed()
 
-    if key[pygame.k_DOWN]:
+    if key[pygame.K_DOWN]:
       self.y -= 1
-    if key[pygame.k_UP]:
+    if key[pygame.K_UP]:
       self.y += 2
 
+pygame.init()
+screen_width =600
+screen_height = 600
+screen = pygame.display.set_mode((screen_width,screen_height))
 
+sprite= paddle()
 
 running= True
-paddle= paddle()
+
 
 while running:
+  for event in pygame.event.get():
+    if event.type== pygame.QUIT:
+      pygame.quit()
+      running= False
 
-  color = (555,555,555)
-  pygame.init()
-  screen_width =600
-  screen_height = 600
-  screen = pygame.display.set_mode((screen_width,screen_height))
+  screen.fill((255,255,255))
+
+  sprite.movement()
+
+  sprite.draw(screen)
 
   pygame.display.update()
